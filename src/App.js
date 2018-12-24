e8794d24bbc2fbb24d8d91e0736cdf1fbf66fa3a
karlkras <karlkras@yahoo.com>
2018-12-24 09:18:38 -0800

    even more adjustments

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { Header } from './components/common';
+import ducers from './reducers';
 
 const App = () => {
     return (
-        <Provider store={createStore(() => [])} >
+        <Provider store={createStore(ducers)} >
             <Fragment>
                 <Header headerText={'This is some text'} />
             </Fragment>
